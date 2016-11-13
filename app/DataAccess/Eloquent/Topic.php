9a0e9f6f686aa7443debbb277b56207ea8dd6325
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-11-14 00:03:22 +0900

    TopicのEloquentモデルを作成 php artisan make:model DataAccess/Eloquent/Topic

---
+++ app/DataAccess/Eloquent/Topic.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataAccess\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'topics';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'title', 'body', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo('\App\DataAccess\Eloquent\User');
+    }
+}
