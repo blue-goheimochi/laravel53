ae0ee8bfc64cac7f526283924a84686b605d37ae
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-11-25 14:29:13 +0900

    likesテーブル作成のマイグレーションファイル作成 php artisan make:migration create_likes_table

---
+++ database/migrations/2016_11_25_142706_create_likes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('topic_id')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->unique(array('user_id', 'topic_id'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('likes');
+    }
+}
